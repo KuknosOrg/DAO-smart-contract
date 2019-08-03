20e116f2ed0c634b41ab842ddf29efd85e64a7e7
javadkh2 <khalilian.javad@gmail.com>
2019-08-03 14:01:40 +0430

    dao test

--- test/KuknosDAO.spec.js
+++ test/KuknosDAO.spec.js
@@ -12,7 +12,32 @@ contract("KuknosDAO", (accounts) => {
             .then(instance => ct = instance)
             .then(ct => ct.balanceOf(accounts[1]))
             .then(balance => assert.equal(balance, 50))
-            .then(() => ct.registerAddAnchorProposal("anchor", "http://anchor.test", [accounts[8], accounts[9]], addDay(-1)(now()), addDay(8)(now()), "http://test.com", "123", {
+            .then(() => ct.registerAddOrUpdateAnchorProposal("test", "http://anchor.test", [accounts[8], accounts[9]], addDay(-1)(now()), addDay(0)(now() + 5000), "http://test.com", "123", 0, {
+                from: accounts[1]
+            }))
+            .then(() => ct.getProposal(0))
+            .then((proposal) => assert.equal(proposal.title, "add anchor"))
+            .then(() => ct.balanceOf(accounts[1]))
+            .then(balance => assert.equal(balance, 49))
+            .then(() => ct.voteForProposal(0, 1, { from: accounts[1] }))
+            .then(() => ct.voteForProposal(0, 1, { from: accounts[2] }))
+            .then(() => ct.voteForProposal(0, 1, { from: accounts[3] }))
+            .then(() => ct.voteForProposal(0, 1, { from: accounts[4] }))
+            .then(() => wait(7000))
+            .then(() => ct.runAddOrUpdateAnchorProposal(0, 0, { from: accounts[1] }))
+            .then(() => ct.anchors(2))
+            .then((anchor) => assert.equal(anchor.name, "test"))
+            .then(() => ct.membersList())
+            .then((list) => assert.equal(list.length, 6))
+    }, 20000)
+
+    it("registerRemoveAnchorProposal", () => {
+        let ct;
+        return getContract(DAO, accounts)
+            .then(instance => ct = instance)
+            .then(ct => ct.balanceOf(accounts[1]))
+            .then(balance => assert.equal(balance, 50))
+            .then(() => ct.registerAddOrUpdateAnchorProposal("anchor", addDay(-1)(now()), addDay(0)(now()), "http://test.com", "123", 0, {
                 from: accounts[1]
             }))
             .then(() => ct.getProposal(0))
